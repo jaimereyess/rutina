35a020fd21a26628009f9c62886fb66af609ecc9
jaimereyess <jaimereyesrdgz@gmail.com>
2023-09-16 00:26:20 +0100

    despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,19 @@ import Friday from "./pages/friday";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/monday" element={<Monday />} />
-        <Route path="/tuesday" element={<Tuesday />} />
-        <Route path="/wednesday" element={<Wednesday />} />
-        <Route path="/thursday" element={<Thursday />} />
-        <Route path="/friday" element={<Friday />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/monday" element={<Monday />} />
+          <Route path="/tuesday" element={<Tuesday />} />
+          <Route path="/wednesday" element={<Wednesday />} />
+          <Route path="/thursday" element={<Thursday />} />
+          <Route path="/friday" element={<Friday />} />
+        </Routes>
+      </BrowserRouter>
+      <h1>prueba</h1>
+    </>
   );
 }
 
