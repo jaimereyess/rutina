3fa0b6387964603af6d651a37d4aca2ededb97e3
jaimereyess <jaimereyesrdgz@gmail.com>
2023-09-19 00:05:42 +0100

    prueba de despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const isProduction = process.env.NODE_ENV === 'production'
-
 export default defineConfig({
   plugins: [react()],
-  base: isProduction ? '/dist' : '',
+  base: 'https://jaimereyess.github.io/rutina/',
   build: {
     outDir: 'dist'
 
