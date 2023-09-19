05569521b6c4907a2d2f7841a5652f03b108157d
jaimereyess <jaimereyesrdgz@gmail.com>
2023-09-19 12:04:39 +0100

    prueba de despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import HomePage from "./pages/homePage";
-// import Monday from "./pages/monday";
-// import Tuesday from "./pages/tuesday";
-// import Wednesday from "./pages/wednesday";
-// import Thursday from "./pages/thursday";
-// import Friday from "./pages/friday";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import Monday from "./pages/monday";
+import Tuesday from "./pages/tuesday";
+import Wednesday from "./pages/wednesday";
+import Thursday from "./pages/thursday";
+import Friday from "./pages/friday";
+import Nav from "./Components/nav";
 
 function App() {
   return (
-    <h1>Page</h1>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<HomePage />} />
-    //     <Route path="/monday" element={<Monday />} />
-    //     <Route path="/tuesday" element={<Tuesday />} />
-    //     <Route path="/wednesday" element={<Wednesday />} />
-    //     <Route path="/thursday" element={<Thursday />} />
-    //     <Route path="/friday" element={<Friday />} />
-    //   </Routes>
-    // </BrowserRouter>
+    <>
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/monday" element={<Monday />} />
+          <Route path="/tuesday" element={<Tuesday />} />
+          <Route path="/wednesday" element={<Wednesday />} />
+          <Route path="/thursday" element={<Thursday />} />
+          <Route path="/friday" element={<Friday />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jaimereyess.github.io/rutina/',
+  base: isProduction ? 'https://jaimereyess.github.io/rutina/' : '',
   build: {
     outDir: 'dist'
 
