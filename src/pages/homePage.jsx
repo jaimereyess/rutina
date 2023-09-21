02717ec34124f0594f077adb9d66c2d993a15ffc
jaimereyess <jaimereyesrdgz@gmail.com>
2023-09-21 22:31:59 +0100

    add sidebar and favicon

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,18 +1,20 @@
-import Nav from "../Components/nav";
+import Sidebar from "../Components/sidebar";
 import LeterDays from "../Components/days";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../Components/ui/card"
+import { buttonVariants } from "../Components/ui/button";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className=" min-h-screen bg-darkgrey text-white">
-      <Nav />
+      <Sidebar />
       <LeterDays day="HomePage" />
+      <div className="text-center mt-4">
+
+        <Link className={buttonVariants({ variant: "ghost" }) + " p-10 rounded-md"} to="/monday">Lunes</Link>
+
+      </div>
+      <h1 className="text-blue-600sla">HOlaaa</h1>
+
     </div>
   );
 }
