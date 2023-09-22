c0cc3de47f71c7b2b5b7c2b6258a6a30f9f8ad0d
jaimereyess <jaimereyesrdgz@gmail.com>
2023-09-22 20:04:04 +0100

    prepare deploy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jaimereyess.github.io/rutina',
-  build: {
-    outDir: 'dist'
-
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  }
 })
