c280183a7895e46055a9dbe05e3f7d6b012a0684
jaimereyess <jaimereyesrdgz@gmail.com>
2023-09-22 23:14:23 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://rutina-ten.vercel.app/',
+  build: {
+    outDir: 'dist'
+
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
 })
