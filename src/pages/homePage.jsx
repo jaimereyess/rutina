b5071f968aa48f6b8e0ad55304a67545cb81e0a8
jaimereyess <jaimereyesrdgz@gmail.com>
2023-09-23 22:17:30 +0100

    update Homepage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -8,7 +8,7 @@ function HomePage() {
     <div className=" min-h-screen bg-darkgrey text-white">
       <Nav />
       <LeterDays day="HomePage" />
-      <div className="text-center mt-4">
+      <div className="text-center mt-4 grid lg:block">
 
         <Link className={buttonVariants({ variant: "ghost" }) + " p-10 rounded-md"} to="/monday">Lunes</Link>
         <Link className={buttonVariants({ variant: "ghost" }) + " p-10 rounded-md"} to="/tuesday">Martes</Link>
