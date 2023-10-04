21451887cff76585b86e73cbc00ef545f5bdd77d
Jaime Reyes <126992552+jaimereyess@users.noreply.github.com>
2023-10-04 23:46:20 +0100

    mejor dark toggle

--- src/components/dark.jsx
+++ src/components/dark.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { SVGSun, SVGMoon } from "./svg"
 
 const DarkToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
@@ -26,14 +27,16 @@ const DarkToggle = () => {
     };
 
     return (
-        <div className={`bg-${isDarkMode ? 'black' : 'white'} text-${isDarkMode ? 'white' : 'black'} transition duration-500 ease-in-out dark:bg-gray-800 dark:text-white`}>
+        <div className={"transition duration-500 ease-in-out"}>
             <button
                 onClick={toggleDarkMode}
-                className={`px-4 py-2 rounded-full ${isDarkMode ? 'bg-yellow-400' : 'bg-gray-800'
+                className={`px-4 py-2 rounded-full ${isDarkMode ? 'bg-yellow-400' : 'bg-gray-200'
                     } ${isDarkMode ? 'text-gray-900' : 'text-white'
                     } transition-colors duration-200`}
             >
-                {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+                {isDarkMode
+                    ? <SVGSun />
+                    : <SVGMoon />}
             </button>
         </div>
     );
