c7f99fce6ac1cfad86506ff2632ccb392f1d691d
Jaime Reyes <126992552+jaimereyess@users.noreply.github.com>
2023-10-08 23:47:09 +0100

    profile update

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -9,6 +9,7 @@ function Profile() {
         return (
             <main className="flex min-h-screen flex-col items-center dark:bg-black dark:text-white">
                 <span className="text-6xl font-bold p-20 underline">
+                    <LoginBtn />
                     Bienvenido {session.user.name}
                     <UserImage img={session.user.image} w={50} h={50} />
                 </span>
