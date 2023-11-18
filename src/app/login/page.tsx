e1c49d7991fa2eb4d4c3c5d1d69c2684fa97e50a
Jaime Reyes <126992552+jaimereyess@users.noreply.github.com>
2023-11-18 17:28:48 +0000

    error fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import LoginBtn from "../../components/login-btn";
 function Login() {
     return (
         <div className='flex items-center justify-center'>
-            <LoginBtn provider="google">Sign in with Google
+            <LoginBtn provider="google" bg="bg-sky-400" bgHover="hover:bg-sky-500">Sign in with Google
             </LoginBtn>
         </div>
     )
